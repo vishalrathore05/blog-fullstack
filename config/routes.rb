1f04d6403db30ce8b32cba6957de43026c7fdaab
vishal1234568 <vishalrathod00143@gmail.com>
2022-12-17 17:39:01 +0530

    gvefge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
   Rails.application.routes.draw do
     devise_for :users
-
+   
    resources :comments
   resources :posts
    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
